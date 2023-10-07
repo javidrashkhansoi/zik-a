5ac1e4aede6bd5c3c894d99d24a99740404df35e
Javid <cavidgulmaliyev491995@gmail.com>
2023-10-08 03:52:05 +0400

    add modals

--- js/script.js
+++ js/script.js
@@ -892,6 +892,9 @@ class Burger {
 
 const burger = new Burger({
   breakpoint: 1280,
+  a11y: {
+    inertElementsSelectors: "[data-wrapper] > *:not([data-burger=\"wrapper\"], .dialog)",
+  },
 });
 
 /** @type {HTMLButtonElement} */
@@ -1062,7 +1065,121 @@ if (searchButton && searchForm) {
   });
 }
 
+;// CONCATENATED MODULE: ./src/js/modules/dialogs.js
+
+
+
+const { burger: dialogs_burger, dialogs: dialogs_app, html: { htmlClassList: dialogs_htmlClassList } } = App;
+
+class Dialogs {
+  #selectors = {
+    buttons: "[data-dialog-id]",
+    closeButton: "[data-dialog=\"close\"]",
+    inner: "[data-dialog=\"inner\"]"
+  }
+  #addingClass = "dialog-active";
+  /** @type {NodeListOf<HTMLButtonElement>} */
+  #buttons = document.querySelectorAll(this.#selectors.buttons);
+  /** @type {DialogsObject} */
+  #dialogs = {};
+  #dialogsArray;
+  #onClickDialog = this.#dialogClickEvent.bind(this);
+  #onCloseDialog = this.#dialogCloseEvent.bind(this);
+
+  constructor() {
+    this.#buttons?.forEach(button => {
+      const { dialogId } = button.dataset;
+
+      if (dialogId) {
+        /** @type {HTMLDialogElement} */
+        const dialog = document.getElementById(dialogId);
+
+        if (dialog) {
+          button.setAttribute("aria-controls", dialogId);
+          button.ariaExpanded = false;
+
+          this.#dialogs[dialogId] = {
+            $button: button,
+            $dialog: dialog,
+            isActive: false
+          };
+        }
+      }
+    });
+
+    this.#dialogsArray = Object.values(this.#dialogs);
+
+    if (this.#dialogsArray.length) {
+      this.#init();
+    }
+  }
+
+  #init() {
+    this.#dialogsArray.forEach(dialog => {
+      const { $button, $dialog } = dialog;
+      /** @type {HTMLButtonElement} */
+      const closeButton = $dialog.querySelector(this.#selectors.closeButton);
+
+      $button.addEventListener("click", () => {
+        if (!$button.hasAttribute("data-disabled")) {
+          $dialog.showModal();
+          $button.ariaExpanded = true;
+          $dialog.addEventListener("click", this.#onClickDialog);
+          $dialog.addEventListener("close", this.#onCloseDialog);
+          dialog.isActive = true;
+
+          if (closeButton) closeButton.focus();
+
+          if (!dialogs_app.activeDialogs) {
+            if (!dialogs_burger.isActive) Scrolling.lock();
+            dialogs_htmlClassList.add(this.#addingClass);
+          }
+
+          dialogs_app.activeDialogs++;
+        }
+      });
+    });
+  }
+
+  /** @param {MouseEvent} event */
+  #dialogClickEvent(event) {
+    const { target } = event;
+    /** @type {HTMLDialogElement} */
+    const dialog = target.closest("dialog");
+
+    if (!target.closest(this.#selectors.inner) || target.closest(this.#selectors.closeButton)) dialog.close();
+  }
+
+  /** @param {Event} event */
+  #dialogCloseEvent(event) {
+    const { target } = event;
+    const { id } = target;
+    const { [id]: dialog } = this.#dialogs;
+    const { $button, $dialog } = dialog;
+
+    $button.ariaExpanded = false;
+    $button.focus();
+    $dialog.removeEventListener("click", this.#onClickDialog);
+    $dialog.removeEventListener("close", this.#onCloseDialog);
+    dialog.isActive = false;
+    dialogs_app.activeDialogs--;
+
+    if (!dialogs_app.activeDialogs) {
+      if (!dialogs_burger.isActive) Scrolling.unlock();
+      dialogs_htmlClassList.remove(this.#addingClass);
+    }
+  }
+}
+
+
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/dialogs.js
+
+
+const dialogs_dialogs = new Dialogs();
+
 ;// CONCATENATED MODULE: ./src/js/scripts/scripts.js
+
 
 
 
